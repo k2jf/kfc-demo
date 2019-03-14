4c3e52f3b7ffcda569c9ff0c979469f7c359824d
zhanghao <1891934@qq.com>
2019-03-14 15:43:43 +0800

    Modify menu

---
+++ src/config/menu.js
@@ -0,0 +1,51 @@
+module.exports = {
+    headerMenu: [
+      {
+        name: 'top-menu1',
+        icon: 'md-person',
+        title: '导航菜单1'
+      },
+      {
+        name: 'top-menu2',
+        icon: 'md-person',
+        title: '导航菜单2'
+      }
+    ],
+    siderMenuMap: {
+      'top-menu1': [
+        {
+          name: 'home',
+          icon: 'md-person',
+          title: '左侧一级菜单1'
+        },
+        {
+          name: 'page2',
+          icon: 'md-person',
+          title: '左侧一级菜单2'
+        }
+      ],
+      'top-menu2': [
+        {
+          name: 'page3',
+          icon: 'md-person',
+          title: '左侧一级菜单3'
+        },
+        {
+          icon: 'md-person',
+          title: '左侧一级菜单4',
+          children: [
+            {
+              name: 'page4',
+              icon: 'md-person',
+              title: '左侧二级菜单1'
+            },
+            {
+              name: 'page4',
+              icon: 'md-person',
+              title: '左侧二级菜单2'
+            }
+          ]
+        }
+      ]
+    }
+  }
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'demo',
+      component: KFCFrame,
+      chilren: [
+        {
+          path: '/home',
+          name: 'home',
+          component: Home
+        },
+        {
+          path: '/page2',
+          name: 'page2',
+          component: Page2
+        }
+      ]
+    }
+  ]
+})
