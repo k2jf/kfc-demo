7fbd4836329d1b9a414c1b0a35fcb3cac238ea73
wangliheng <wangliheng@k2data.com.cn>
2019-04-17 15:31:21 +0800

    Squashed 'src/components/kfc-audit/' content from commit 0ebfc63
    git-subtree-dir: src/components/kfc-audit
    git-subtree-split: 0ebfc634269413e25fd976d0dbe27f1d7808b5c3

---
+++ KfcAudit.vue
@@ -0,0 +1,125 @@
+<template>
+  <Card style="width: 90%;margin-left: 5%;margin-top: 2%">
+    <p slot="title">
+      审计日志
+    </p>
+    <div style="margin-top: 1px">
+      <span style="width: 15em;">
+        <span class="four-char">操作用户</span>
+        <Input
+          class="four-char-out"
+          placeholder="请输入用户名称"
+          v-model="searchContent"></Input>
+      </span>
+      <span style="width: 21em;margin-left: 20px">
+        <span class="four-char">操作时间</span>
+        <DatePicker
+          type="daterange"
+          class="four-char-out"
+          v-model="dateRange"></DatePicker>
+      </span>
+      <span style="width: 9em; margin-left: 20px">
+        <Button type="primary" @click.native="onSearch">查询</Button>
+        <Button style="margin-left: 5px" @click.native="onReset">重置</Button>
+      </span>
+    </div>
+    <div style="margin-top: 20px">
+      <KfcTable
+        :headers="table.headers"
+        :datas="table.dataList"
+        :total="table.total"
+        ref="table"
+        @on-page-change="onPageChange" />
+    </div>
+  </Card>
+</template>
+
+<script>
+import { Card, Input, DatePicker, Button } from 'iview'
+import KfcTable from './KfcTable'
+export default {
+  name: 'KfcAudit',
+  components: {
+    Card,
+    Input,
+    DatePicker,
+    Button,
+    KfcTable
+  },
+  data () {
+    return {
+      searchContent: '',
+      dateRange: [],
+      table: {
+        headers: [
+          { title: '操作标题', key: 'title' },
+          { title: '请求地址', key: 'requestURL' },
+          { title: '请求方式', key: 'requestMethod' },
+          { title: '操作用户', key: 'creator' },
+          { title: '操作用户IP', key: 'ip' },
+          { title: '操作时间', key: 'createDate' }
+        ],
+        dataList: [],
+        pageNo: 1,
+        pageSize: 10,
+        total: 6
+      }
+    }
+  },
+  mounted () {
+    this.reloadTable()
+  },
+  methods: {
+    // todo 对接数据
+    reloadTable () {
+      let param =
+        {
+          creator: this.searchContent,
+          createStartDate: this.dateRange[0],
+          createEndDate: this.dateRange[1],
+          pageNo: this.table.pageNo,
+          size: this.table.pageSize
+        }
+      this.$axios.get('/auditLogList', param).then(res => {
+        this.table.dataList = res.body.audiLogList
+        /* this.table.total = res.body. */
+      })
+    },
+    onSearch () {
+      this.reloadTable()
+    },
+    onReset () {
+      this.searchContent = ''
+      this.dateRange = []
+      this.reloadTable()
+    },
+    onPageChange (pageNo, pageSize) {
+      this.table.pageNo = pageNo
+      this.table.pageSize = pageSize
+      this.reloadTable()
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .four-char {
+    display: inline-block;
+    text-align: right;
+    width: 5em;
+    margin-right: 0.5em;
+    margin-top: 3px;
+    vertical-align: text-top;
+  }
+  .four-char:after {
+    content: ' :'
+  }
+
+  .four-char-out {
+    width: calc( 100% - 6.5em );
+    height: auto;
+    display: inline-block;
+    vertical-align: top;
+    width: 200px;
+  }
+</style>
